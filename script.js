c0133947eca5906de5406a2af0282f64fbdd2d9f
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-20 21:36:54 -0300

    feat: Adicionadas funções que limppam e criam o board

--- script.js
+++ script.js
@@ -6,10 +6,26 @@ const clear = document.getElementById('clear');
 const generatePalette = document.getElementById('generat-palette');
 const paletteSize = document.getElementById('palette-size');
 const randoPalette = document.getElementById('randon-palette');
+const board = document.getElementById('board');
+
+const clearBoard = () => {
+  document.querySelectorAll('.pixel').forEach((pixel) => pixel.remove());
+}
+
+const generateBoard = () => {
+  clearBoard();
+  const number = parseInt(inputSizeValue.innerText);
+  for(let index = 0; index < number ; index +=1) {
+    const element = document.createElement('div');
+    element.className = 'pixel';
+    board.appendChild(element)
+  }
+}
 
 const changeInputSizeValue = () => {
   const newValue = inputSize.value;
   inputSizeValue.innerText = newValue;
+  generateBoard();
 }
 
 const changeSampleBackgroundColor = () => {
@@ -21,5 +37,6 @@ inputSize.addEventListener('change', changeInputSizeValue);
 inputColor.addEventListener('change', changeSampleBackgroundColor);
 
 window.onload = () => {
-  changeSampleBackgroundColor()
+  changeSampleBackgroundColor();
+  generateBoard();
 };
