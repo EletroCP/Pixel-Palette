773f16a7e83463470f51f6cc1e609f069e6c77f4
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-22 08:27:46 -0300

    feat: Adicionada função que gera a paleta padrão

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const inputSizeValue = document.getElementById('input-size-value');
 const inputSize = document.getElementById('input-size');
 const inputColor = document.getElementById('input-color');
-const sample = document.getElementById('sample');
+const sample = document.querySelector('.sample');
 const clear = document.getElementById('clear');
 const generatePalette = document.getElementById('generat-palette');
 const paletteSize = document.getElementById('palette-size');
 const randoPalette = document.getElementById('randon-palette');
 const board = document.getElementById('board');
+const palette = document.getElementById('palette');
+const standardSamples = document.getElementById('standard-samples');
 
 let color = 'black';
 
@@ -26,6 +28,27 @@ const generateBoard = () => {
     element.className = 'pixel';
     element.addEventListener('click', paint);
     board.appendChild(element);
+  }
+}
+
+const generatepalette = () => {
+  for(let index = 0; index < 6 ; index +=1) {
+    const element = document.createElement('div');
+    element.className = 'sample';
+    element.addEventListener('click', paint);
+    palette.appendChild(element);
+  }
+}
+
+const generateColorPaletteDefault = () => {
+  const colors = ['black', 'white', 'red', 'green', 'blue']
+  for(let index = 0; index < 5 ; index +=1) {
+    const element = document.createElement('div');
+    element.className = 'sample';
+    element.id = colors[index];
+    element.style.backgroundColor = colors[index];
+    element.addEventListener('click', paint);
+    standardSamples.appendChild(element);
   }
 }
 
@@ -52,9 +75,10 @@ const getColor = () => {
 sample.addEventListener('click', getColor)
 clear.addEventListener('click', resetBoard);
 inputSize.addEventListener('change', changeInputSizeValue);
-inputColor.addEventListener('change', changeSampleBackgroundColor);
-sample
+inputColor.addEventListener('change', changeSampleBackgroundColor, getColor);
 window.onload = () => {
   changeSampleBackgroundColor();
   generateBoard();
+  generatepalette();
+  generateColorPaletteDefault();
 };
